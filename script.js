ca240bedc6f6efae8315f0f9f72fc993b901707a
k1yeon <gio13remanente@gmail.com>
2021-03-21 09:48:46 +0800

    Added debouncer function

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
 const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cities = [];
+
+const debounce = (func, wait, immediate) => {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const context = this;
+    const later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
 
 const getJSON = async () => {
   const res = await fetch(endpoint);
@@ -11,20 +29,48 @@ const getJSON = async () => {
 
 getJSON();
 
+const numberWithCommas = function (x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
 const findMatches = function (wordToMatch, cities) {
   return cities.filter(place => {
-    // if the city or state matches what was searched
     const regex = new RegExp(wordToMatch, 'gi');
     return place.city.match(regex) || place.state.match(regex);
   });
 };
 
 const displayMatches = function () {
+  let html;
   const matchArray = findMatches(this.value, cities);
-  console.log(matchArray);
+  if (Array.isArray(matchArray) && !matchArray.length) {
+    html = `
+      <li>
+        <span class="name">No Results Found</span>
+      </li>
+    `;
+  } else {
+    html = matchArray
+      .map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(
+          regex,
+          `<span class="hl">${this.value}</span>`
+        );
+        const stateName = place.state.replace(
+          regex,
+          `<span class="hl">${this.value}</span>`
+        );
+        return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+      })
+      .join('');
+  }
+  suggestions.innerHTML = html;
 };
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('input', displayMatches);
+searchInput.addEventListener('input', debounce(displayMatches, 300));
